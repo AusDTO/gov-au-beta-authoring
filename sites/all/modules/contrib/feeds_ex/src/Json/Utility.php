af49375fb5b6e2db3f91c4335d24e5f203afa6a3
Alexander Sadleir <maxious@lambdacomplex.org>
2016-04-05 15:37:46 +1000

    Add JSON based taxonomy feed

---
+++ sites/all/modules/contrib/feeds_ex/src/Json/Utility.php
@@ -0,0 +1,186 @@
+<?php
+
+/**
+ * @file
+ * Contains FeedsExJsonUtility.
+ */
+
+/**
+ * Various helpers for handling JSON.
+ */
+class FeedsExJsonUtility {
+
+  /**
+   * Translates an error message.
+   *
+   * @param int $error
+   *   The JSON error.
+   *
+   * @return string
+   *   The JSON parsing error message.
+   */
+  public static function translateError($error) {
+    // This shouldn't get called for PHP < 5.3.0.
+    if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+      return 'Unknown error';
+    }
+
+    if (version_compare(PHP_VERSION, '5.3.3', '>=')) {
+      switch ($error) {
+        case JSON_ERROR_UTF8:
+          return 'Malformed UTF-8 characters, possibly incorrectly encoded';
+      }
+    }
+
+    if (version_compare(PHP_VERSION, '5.5.0', '>=')) {
+      switch ($error) {
+        case JSON_ERROR_RECURSION:
+          return 'One or more recursive references in the value to be encoded';
+
+        case JSON_ERROR_INF_OR_NAN:
+          return 'One or more NAN or INF values in the value to be encoded';
+
+        case JSON_ERROR_UNSUPPORTED_TYPE:
+          return 'A value of a type that cannot be encoded was given';
+      }
+    }
+
+    switch ($error) {
+      case JSON_ERROR_NONE:
+        return 'No error has occurred';
+
+      case JSON_ERROR_DEPTH:
+        return 'The maximum stack depth has been exceeded';
+
+      case JSON_ERROR_STATE_MISMATCH:
+        return 'Invalid or malformed JSON';
+
+      case JSON_ERROR_CTRL_CHAR:
+        return 'Control character error, possibly incorrectly encoded';
+
+      case JSON_ERROR_SYNTAX:
+        return 'Syntax error';
+
+      default:
+        return 'Unknown error';
+    }
+  }
+
+  /**
+   * Decodes a JSON string into an array.
+   *
+   * @param string $json
+   *   A JSON string.
+   *
+   * @return array
+   *   A PHP array.
+   *
+   * @throws RuntimeException
+   *   Thrown if the encoded JSON does not result in an array.
+   */
+  public static function decodeJsonArray($json) {
+    $parsed = drupal_json_decode($json);
+
+    if (!is_array($parsed)) {
+      throw new RuntimeException(t('The JSON is invalid.'));
+    }
+
+    return $parsed;
+  }
+
+  /**
+   * Decodes a JSON string into an object or array.
+   *
+   * @param string $json
+   *   A JSON string.
+   *
+   * @return object|array
+   *   A PHP object or array.
+   *
+   * @throws RuntimeException
+   *   Thrown if the encoded JSON does not result in an array or object.
+   */
+  public static function decodeJsonObject($json) {
+    $parsed = json_decode($json);
+
+    if (!is_array($parsed) && !is_object($parsed)) {
+      throw new RuntimeException(t('The JSON is invalid.'));
+    }
+
+    return $parsed;
+  }
+
+  /**
+   * Returns the path of the JSONPath library.
+   *
+   * @return string|false
+   *   The root-relative path to the JSONPath include, or false on failure.
+   */
+  public static function jsonPathLibraryPath() {
+    $libraries_path = module_exists('libraries') ? libraries_get_path('jsonpath') : FALSE;
+    if ($libraries_path && is_dir($libraries_path)) {
+      $path = $libraries_path;
+    }
+    elseif (is_dir(DRUPAL_ROOT . '/sites/all/libraries/jsonpath')) {
+      $path = 'sites/all/libraries/jsonpath';
+    }
+    // This is defined when simpletest downloads the library for us.
+    elseif (defined('FEEDS_EX_LIBRARY_PATH')) {
+      $path = FEEDS_EX_LIBRARY_PATH . '/jsonpath';
+    }
+
+    if (!isset($path)) {
+      return FALSE;
+    }
+
+    // Newer forks of JSONPath are all modern and fancy with their autoloaders.
+    if (is_file($path . '/vendor/autoload.php')) {
+      return $path . '/vendor/autoload.php';
+    }
+    // Old school. Look for multiple versions.
+    $files = glob($path . '/jsonpath*.php');
+
+    return $files ? reset($files) : FALSE;
+  }
+
+  /**
+   * Determines if a JSONPath parser is installed.
+   *
+   * @return bool
+   *   True if a parser is installed, false if not.
+   */
+  public static function jsonPathParserInstalled() {
+    if (class_exists('Flow\JSONPath\JSONPath') || function_exists('jsonPath')) {
+      return TRUE;
+    }
+
+    if (!$path = self::jsonPathLibraryPath()) {
+      return FALSE;
+    }
+
+    require_once DRUPAL_ROOT . '/' . $path;
+
+    return class_exists('Flow\JSONPath\JSONPath') || function_exists('jsonPath');
+  }
+
+  /**
+   * Determines if a JMESPath parser is installed.
+   *
+   * @return bool
+   *   True if a parser is installed, false if not.
+   */
+  public static function jmesPathParserInstalled() {
+    if (class_exists('JmesPath\AstRuntime') || class_exists('JmesPath\Runtime\AstRuntime')) {
+      return TRUE;
+    }
+
+    if (!$path = feeds_ex_library_path('jmespath.php', 'vendor/autoload.php')) {
+      return FALSE;
+    }
+
+    require_once DRUPAL_ROOT . '/' . $path;
+
+    return class_exists('JmesPath\AstRuntime') || class_exists('JmesPath\Runtime\AstRuntime');
+  }
+
+}
